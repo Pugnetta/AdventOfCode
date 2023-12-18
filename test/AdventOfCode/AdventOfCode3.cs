34c76cb72b79b10e89548fde1a3b972eb43de094
Pugnetta <115831639+Pugnetta@users.noreply.github.com>
2023-12-18 03:55:40 +0100

    Day3

--- test/AdventOfCode/AdventOfCode3.cs
+++ test/AdventOfCode/AdventOfCode3.cs
@@ -1,25 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using test.AdventOfCode.Data;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace test.AdventOfCode
 {
     internal class AdventOfCode3
     {
-        private static string[,] ParseData(string s)
+        private readonly char[,] _grid = ParseData(Day3.Data);
+        private char this[int x, int y]
+        {
+            get { return _grid[x, y]; }           
+        }
+        private static char[,] ParseData(string s)
         {
             string[] data = s.Split("\r\n");
-            string[,] ParsedData
-            for(int i = 0; i < data[0].Length; i++)
+            char[,] ParsedData = new char[data[0].Length, data.Length];
+            for(int i = 0; i < data.Length; i++)
             {
-                for (int j = 0; j < data.Length; j++)
+                //rows
+                for (int j = 0; j < data[i].Length; j++)
                 {
-
+                    //coloms
+                    ParsedData[i, j] = data[i][j];
                 }
             }
+            return ParsedData;
         }
     }
 }
