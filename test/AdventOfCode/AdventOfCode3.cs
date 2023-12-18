5329ea785d495d4e90e9a3cf3eb84659533a8d9b
Pugnetta <115831639+Pugnetta@users.noreply.github.com>
2023-12-18 03:54:49 +0100

    Day3

---
+++ test/AdventOfCode/AdventOfCode3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace test.AdventOfCode
+{
+    internal class AdventOfCode3
+    {
+        private static string[,] ParseData(string s)
+        {
+            string[] data = s.Split("\r\n");
+            string[,] ParsedData
+            for(int i = 0; i < data[0].Length; i++)
+            {
+                for (int j = 0; j < data.Length; j++)
+                {
+
+                }
+            }
+        }
+    }
+}
